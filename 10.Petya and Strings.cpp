885e638bc81ef256a0333c1eb04d87833f527f10
Bayazid Hasan <78470516+hbayazid969@users.noreply.github.com>
2022-08-08 13:34:35 +0600

    Create 10.Petya and Strings.cpp

---
+++ 10.Petya and Strings.cpp
@@ -0,0 +1,32 @@
+/*
+https://codeforces.com/contest/112/problem/A
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s1,s2;
+    cin>>s1>>s2;
+    for(int i = 0 ; i< s1.size();i++){
+        s1[i]=tolower(s1[i]);
+        s2[i]=tolower(s2[i]);
+    }
+    if(s1 == s2){
+        cout<<"0"<<endl;
+    }
+    else{
+        for(int i = 0;i<s1.size();i++){
+            if(s1[i]<s2[i]){
+                cout<<"-1"<<endl;
+                break;
+            }
+            else if(s1[i]>s2[i]){
+                cout<<"1"<<endl;
+                break;
+            }
+        }
+    }
+ 
+}
